b1870a02bcbf017d533b0a688e6b3972fa293408
Guilherme André Welter <guilherme.a.welter@gmail.com>
2020-04-21 16:45:57 -0300

    Creating appointments using Repository pattern

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentRepository from '../repositories/AppointmentRepository';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentRepository();
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({
+      message: 'This appointment is already booked',
+    });
+  }
+
+  const appointment = appointmentsRepository.create(provider, parsedDate);
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
