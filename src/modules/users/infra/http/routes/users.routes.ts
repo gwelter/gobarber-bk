1a9c69727f6e5d43aeef6ca45bc59bab8a1612fa
Guilherme André Welter <guilherme.a.welter@gmail.com>
2020-06-02 17:11:48 -0300

    Extraíndo codigo da rota para dentro de controllers

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,39 +1,19 @@
 import multer from 'multer';
 import { Router } from 'express';
-import { container } from 'tsyringe';
 import uploadConfig from '@config/upload';
 
-import CreateUserService from '@modules/users/services/CreateUserService';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', async (request, response) => {
-  const { name, email, password } = request.body;
+usersRouter.post('/', usersController.create);
 
-  const createUser = container.resolve(CreateUserService);
-
-  const user = await createUser.execute({ name, email, password });
-
-  delete user.password;
-
-  return response.json(user);
-});
-
-usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-  const updateUserAvatar = container.resolve(UpdateUserAvatarService);
-
-  const user = await updateUserAvatar.execute({
-    user_id: request.user.id,
-    avatarFileName: request.file.filename,
-  });
-
-  delete user.password;
-
-  return response.json({ user });
-});
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), userAvatarController.update);
 
 export default usersRouter;
