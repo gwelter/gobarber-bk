364cba6a7e9806f05e61d427e6277c522f2406e7
Guilherme André Welter <guilherme.a.welter@gmail.com>
2020-06-14 16:57:02 -0300

    Finalizando testes de envio de email com template

---
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -0,0 +1,8 @@
+import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
+import IMailTemplateProvider from '../models/IMailTemplateProvider';
+
+export default class FakeMailTemplateProvider implements IMailTemplateProvider {
+  public async parse({ file }: IParseMailTemplateDTO): Promise<string> {
+    return `Mail Content from file: ${file}`;
+  }
+}
