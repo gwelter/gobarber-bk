e37b79cd22a24c54939d1ea763560fcb08acb80c
Guilherme André Welter <guilherme.a.welter@gmail.com>
2020-06-16 15:03:52 -0300

    Criando serviço de listagem de provedores

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,37 @@
+// import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProvidersService: ListProvidersService;
+
+describe('ShowProfile', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    listProvidersService = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able to list all providers', async () => {
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'Jon Doe',
+      email: 'jon.doe@test.com',
+      password: '123456',
+    });
+
+    const tre = await fakeUsersRepository.create({
+      name: 'Jon Tre',
+      email: 'jon.tre@test.com',
+      password: '123456',
+    });
+
+    const qua = await fakeUsersRepository.create({
+      name: 'Jon Qua',
+      email: 'jon.qua@test.com',
+      password: '123456',
+    });
+
+    const users = await listProvidersService.execute({ user_id: loggedUser.id });
+
+    expect(users).toEqual([tre, qua]);
+  });
+});
---
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -0,0 +1,23 @@
+import { injectable, inject } from 'tsyringe';
+
+// import AppError from '@shared/errors/AppError';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+
+interface IRequest {
+  user_id: string;
+}
+
+@injectable()
+export default class ShowProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ user_id }: IRequest): Promise<User[]> {
+    const users = await this.usersRepository.findAllProviders({ exceptUserId: user_id });
+    return users;
+  }
+}
