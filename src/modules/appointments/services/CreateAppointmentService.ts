2925b122ee34755b3353d488a1d4f493bc6af47e
Guilherme André Welter <guilherme.a.welter@gmail.com>
2020-06-27 12:17:02 -0300

    Configurando cache com redis e aplicando-o

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
 // import {  } from "@modules/notifications/infra/typeorm/repositories/NotificationsRepository";
 import Appointment from '../infra/typeorm/entities/Appointment';
@@ -21,6 +22,8 @@ export default class CreateAppointmentService {
     private appointmentsRepository: IAppointmentsRepository,
     @inject('NotificationsRepository')
     private notificationsRepository: INotificationsRepository,
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ provider_id, user_id, date }: IRequest): Promise<Appointment> {
@@ -55,6 +58,9 @@ export default class CreateAppointmentService {
       recipient_id: provider_id,
       content: `Novo agendamento para dia ${formatedDate}`,
     });
+
+    const cacheKey = `provider-appointments:${provider_id}:${format(appointment.date, 'yyyy-M-d')}`;
+    await this.cacheProvider.invalidate(cacheKey);
 
     return appointment;
   }
--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,5 +1,6 @@
 import { injectable, inject } from 'tsyringe';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
@@ -15,15 +16,23 @@ export default class ListProviderMonthAvailabilityService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ provider_id, day, month, year }: IRequest): Promise<Appointment[]> {
-    const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
-      provider_id,
-      day,
-      month,
-      year,
-    });
+    const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
+    let appointments = await this.cacheProvider.recover<Appointment[]>(cacheKey);
+
+    if (!appointments) {
+      appointments = await this.appointmentsRepository.findAllInDayFromProvider({
+        provider_id,
+        day,
+        month,
+        year,
+      });
+      await this.cacheProvider.save(cacheKey, appointments);
+    }
     return appointments;
   }
 }
