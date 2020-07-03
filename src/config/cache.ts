7fdc7b24ce0dc648828b40191b4067caa03d8876
Guilherme André Welter <guilherme.a.welter@gmail.com>
2020-07-03 17:15:34 -0300

    Limitando requests

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -1,4 +1,4 @@
-import { RedisOptions, RedisOptions } from 'ioredis';
+import { RedisOptions } from 'ioredis';
 
 interface ICacheConfig {
   driver: 'redis';
@@ -12,9 +12,9 @@ export default {
 
   config: {
     redis: {
-      host: 'localhost',
-      port: 6379,
-      password: undefined,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASS || undefined,
     },
   },
 } as ICacheConfig;
